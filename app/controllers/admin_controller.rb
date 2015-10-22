9718df42407827430a007c9c0dc0c21541673137
Joshua Brewton <Joshua.Brewton@sage.com>
2015-10-22 15:06:39 -0400

    privilege escalation - admins for everyone

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  before_action :administrative, :if => :admin_param, :except => [:get_user]
+  before_action :administrative
   skip_before_filter :has_info
 
   def dashboard
